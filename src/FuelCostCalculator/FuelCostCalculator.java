4fd8e91f95dceff14985762a4647ed78acd15f8b
LuvuyoBTech <st10477994@vcconnect.edu.za>
2025-03-20 13:33:57 +0200

    ICE Task One project
    Included is my fuel calculator java ice task

---
+++ src/FuelCostCalculator/FuelCostCalculator.java
@@ -0,0 +1,35 @@
+package FuelCostCalculator;
+
+
+//importing the Scanner class to take input from the user
+import java.util.Scanner;
+
+
+public class FuelCostCalculator {
+    
+        
+    public static void main(String[] args) {
+  //creating an instance of the scanner class      
+    Scanner input = new Scanner(System.in);
+    
+        System.out.println("total distance of the trip in kilometers");
+        int kilometers = input.nextInt();
+        
+        System.out.println("fuel efficiency of their car (km per liter)");
+        double fuelEfficiency = input.nextDouble();
+        
+        System.out.println("price of fuel per liter");
+        double price = input.nextDouble();
+        
+        
+        //formula
+        
+         double fuelNeeded = kilometers/fuelEfficiency;
+         
+        double costOfFuel = fuelNeeded * price;
+        
+        System.out.println("The total amount of Fuel is " + costOfFuel);
+         input.close();
+    }
+    
+}
